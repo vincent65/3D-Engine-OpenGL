2bd54666a11292ed9bfb3e056f45adac9db94a78
Vincent Yip <vincentyip@Vincents-MBP.fios-router.home>
2022-09-05 18:05:51 -0400

    base classes to render a 3D object with the engine

---
+++ mesh.py
@@ -0,0 +1,15 @@
+#handles the vao and the textures
+
+from vao import VAO
+from texture import Texture
+
+
+class Mesh:
+    def __init__(self, app):
+        self.app = app
+        self.vao = VAO(app.ctx)
+        self.texture = Texture(app.ctx)
+
+    def destroy(self):
+        self.vao.destroy()
+        self.texture.destroy()
---
+++ vao.py
@@ -0,0 +1,30 @@
+#Vertex Array Object
+
+from vbo import VBO
+from shaderprogram import ShaderProgram
+
+
+class VAO:
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.vbo = VBO(ctx)
+        self.program = ShaderProgram(ctx)
+        self.vaos = {}
+
+        # cube vao
+        self.vaos['cube'] = self.get_vao(
+            program=self.program.programs['default'],
+            vbo = self.vbo.vbos['cube'])
+
+        # cat vao
+        self.vaos['cat'] = self.get_vao(
+            program=self.program.programs['default'],
+            vbo=self.vbo.vbos['cat'])
+
+    def get_vao(self, program, vbo):
+        vao = self.ctx.vertex_array(program, [(vbo.vbo, vbo.format, *vbo.attribs)])
+        return vao
+
+    def destroy(self):
+        self.vbo.destroy()
+        self.program.destroy()
